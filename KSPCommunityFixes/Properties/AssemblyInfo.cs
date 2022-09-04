feaf243237ac3eeca44b7339d617f23f1606b8ae
NathanKell <nathankell@gmail.com>
2022-09-03 19:14:38 -0700

    Add KSPAssembly attribute

--- KSPCommunityFixes/Properties/AssemblyInfo.cs
+++ KSPCommunityFixes/Properties/AssemblyInfo.cs
@@ -31,3 +31,5 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("1.21.1.0")]
 [assembly: AssemblyFileVersion("1.21.1.0")]
+
+[assembly: KSPAssembly("KSPCommunityFixes", 1, 21, 2)]
